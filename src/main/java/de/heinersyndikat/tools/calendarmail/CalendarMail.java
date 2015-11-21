2e598f6d892b1b9cc84ecf241a81f658516801db
Sven Bauhan <sde@sven.bauhan.name>
2015-11-21 15:41:50 +0100

    Initialized command line parsing

--- src/main/java/de/heinersyndikat/tools/calendarmail/CalendarMail.java
+++ src/main/java/de/heinersyndikat/tools/calendarmail/CalendarMail.java
@@ -2,21 +2,34 @@ package de.heinersyndikat.tools.calendarmail;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.ParseException;
 
 /**
  * Main class.
- * 
+ *
  * @author Sven Bauhan <sde@sven.bauhan.name>
  */
 public class CalendarMail {
 
-	/** Logger instance */
-	private static transient final Logger logger =
-					LoggerFactory.getLogger(Thread.currentThread().getStackTrace()[1].getClassName());
-	/** Application properties */
+	/**
+	 * Logger instance
+	 */
+	private static transient final Logger logger
+					= LoggerFactory.getLogger(Thread.currentThread().getStackTrace()[1].getClassName());
+	/**
+	 * Application properties
+	 */
 	static Properties appProperties = new Properties();
 	final static String APP_PROP_RESSOURCE = "/application.properties";
 
@@ -29,24 +42,69 @@ public class CalendarMail {
 		logger.debug("Application property Stream: " + propertiesStream);
 		try {
 			appProperties.load(propertiesStream);
-		appProperties.stringPropertyNames().stream().forEach((key) -> {
-			String value = appProperties.getProperty(key, "");
-			logger.debug("application property: " + key + " => " + value);
-		});
+			appProperties.stringPropertyNames().stream().forEach((key) -> {
+				String value = appProperties.getProperty(key, "");
+				logger.debug("application property: " + key + " => " + value);
+			});
 		} catch (IOException ex) {
 			logger.warn("Unable to load application properties");
 		}
 	}
-	
+
+	/**
+	 * Define the command line options.
+	 *
+	 * @return the available command line options
+	 */
+	protected static Options defineOptions() {
+		Options options = new Options();
+		Option help = new Option("h", "print this message");
+		options.addOption(help);
+		Option readFile = OptionBuilder.withArgName("file")
+						.hasArg()
+						.withDescription("configuration file to be read")
+						.create("f");
+		options.addOption(readFile);
+		return options;
+	}
+
+	/**
+	 * Parse commandline for given parameters
+	 * 
+	 * @param cmdline commandline with parmeters
+	 */
+	protected static void start(CommandLine cmdline) {
+		if (cmdline.hasOption("h")) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("CalendarMail", defineOptions());
+			System.exit(0);
+		}
+		if (cmdline.hasOption("f")) {
+			logger.debug("found option -f");
+			String filename = cmdline.getOptionValue("f");
+			// @todo: implement
+		}
+	}
+
 	/**
 	 * Main function.
-	 * 
+	 *
 	 * @param args command line options
 	 */
 	public static void main(String[] args) {
+		// load application properties
 		load_properties();
 		String version = appProperties.getProperty("application.version", "");
 		String app_name = appProperties.getProperty("application.name", "CalendarMail");
 		logger.info(app_name + " " + version + ": Sending reminder for calendar entries via email");
+		// parse command line parameters
+		CommandLineParser parser = new BasicParser();
+		try {
+			CommandLine line = parser.parse(defineOptions(), args);
+			start(line);
+		} catch (ParseException ex) {
+			logger.error("Parse Error: " + ex.getLocalizedMessage());
+			System.exit(1);
+		}
 	}
 }
