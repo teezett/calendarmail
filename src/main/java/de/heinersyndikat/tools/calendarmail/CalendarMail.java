cc31619203307d99adeed1bddcb2b9338fe5fc60
Sven Bauhan <sde@sven.bauhan.name>
2015-12-06 22:44:39 +0100

    Try to encrypt and decrypt strings

--- src/main/java/de/heinersyndikat/tools/calendarmail/CalendarMail.java
+++ src/main/java/de/heinersyndikat/tools/calendarmail/CalendarMail.java
@@ -3,6 +3,7 @@ package de.heinersyndikat.tools.calendarmail;
 import com.typesafe.config.ConfigException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.GeneralSecurityException;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -37,6 +38,7 @@ public class CalendarMail {
 	final static String APP_PROP_RESSOURCE = "/application.properties";
 
 	private static Optional<String> to_encrypt = Optional.empty();
+	private static Optional<String> to_decrypt = Optional.empty();
 
 	/**
 	 * Load the application properties.
@@ -84,6 +86,12 @@ public class CalendarMail {
 		OptionBuilder.withDescription("string to encrypt");
 		opt = OptionBuilder.create("e");
 		options.addOption(opt);
+		// -u string decryption
+		OptionBuilder.withArgName("string");
+		OptionBuilder.hasArg(true);
+		OptionBuilder.withDescription("string to unencrypt");
+		opt = OptionBuilder.create("u");
+		options.addOption(opt);
 		// return options definition
 		return options;
 	}
@@ -116,6 +124,10 @@ public class CalendarMail {
 			logger.debug("found option -e");
 			to_encrypt = Optional.of(cmdline.getOptionValue("e"));
 		}
+		if (cmdline.hasOption("u")) {
+			logger.debug("found option -u");
+			to_decrypt = Optional.of(cmdline.getOptionValue("u"));
+		}
 	}
 
 	/**
@@ -124,13 +136,37 @@ public class CalendarMail {
 	 * @param unencrypted  string to be encrypted
 	 */
 	public static void encrypt_string(String unencrypted) {
-		Encryption encryption = new Encryption();
 		try {
+			Encryption encryption = new Encryption();
 			String encrypted = encryption.encrypt(unencrypted);
 			logger.info("Encryption of '" + unencrypted + "' = '" + encrypted + "'");
+			logger.info("Decrypted: " + encryption.decrypt(encrypted));
 			System.exit(0);
 		} catch (NoSuchElementException ex) {
 			logger.error("Unable to encrypt string - no encryption password provided: " + ex.getLocalizedMessage());
+			System.exit(4);
+		} catch (GeneralSecurityException ex) {
+			logger.error("Unable to decrypt string: " + ex.getLocalizedMessage());
+			System.exit(4);
+		}
+	}
+
+	/**
+	 * Decrypt a given string.
+	 * 
+	 * @param encrypted  string to be decrypted
+	 */
+	public static void decrypt_string(String encrypted) {
+		try {
+			Encryption encryption = new Encryption();
+			String decrypted = encryption.encrypt(encrypted);
+			logger.info("Decryption of '" + encrypted + "' = '" + decrypted + "'");
+			System.exit(0);
+		} catch (NoSuchElementException ex) {
+			logger.error("Unable to decrypt string - no encryption password provided: " + ex.getLocalizedMessage());
+			System.exit(4);
+		} catch (GeneralSecurityException ex) {
+			logger.error("Unable to decrypt string: " + ex.getLocalizedMessage());
 			System.exit(4);
 		}
 	}
@@ -157,6 +193,7 @@ public class CalendarMail {
 		}
 		// encrypt given string
 		to_encrypt.ifPresent(CalendarMail::encrypt_string);
+		to_decrypt.ifPresent(CalendarMail::decrypt_string);
 		// load configuration
 		try {
 			CalendarMailConfiguration.INSTANCE.load();
--- src/main/java/de/heinersyndikat/tools/calendarmail/Encryption.java
+++ src/main/java/de/heinersyndikat/tools/calendarmail/Encryption.java
@@ -1,28 +1,97 @@
 package de.heinersyndikat.tools.calendarmail;
 
-import java.util.NoSuchElementException;
-import org.jasypt.encryption.pbe.PBEStringEncryptor;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Password encryption in configuration files.
- * 
+ *
  * @author Sven Bauhan <sde@sven.bauhan.name>
  */
 public class Encryption {
 
-	/** Logger instance */
-	private static transient final Logger logger =
-					LoggerFactory.getLogger(Thread.currentThread().getStackTrace()[1].getClassName());
+	/**
+	 * Logger instance
+	 */
+	private static transient final Logger logger
+					= LoggerFactory.getLogger(Thread.currentThread().getStackTrace()[1].getClassName());
 
-	PBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+	private final StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+	private final static String DEFAULT_SALT = "12345678";
+	private final PBEParameterSpec pbeParamSpec;
+	private final SecretKey pbeKey;
+	private final Cipher cipher;
 
-	String encrypt(String string_to_enc) throws NoSuchElementException {
+	/**
+	 * Standard constructor.
+	 *
+	 * Initializes the encryptor.
+	 * @throws java.security.NoSuchAlgorithmException
+	 * @throws java.security.spec.InvalidKeySpecException
+	 * @throws javax.crypto.NoSuchPaddingException
+	 */
+	public Encryption() throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
 		String password = CalendarMailConfiguration.INSTANCE.getPassword();
-		encryptor.setPassword(password);
-		return encryptor.encrypt(string_to_enc);
+		pbeParamSpec = new PBEParameterSpec(DEFAULT_SALT.getBytes(), 20);
+		// Convert password to SecretKey object
+		SecretKeyFactory keyFac = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+		PBEKeySpec pbeKeySpec = new PBEKeySpec(password.toCharArray());
+		pbeKey = keyFac.generateSecret(pbeKeySpec);
+		// Create PBE Cipher
+		cipher = Cipher.getInstance("PBEWithMD5AndDES");
 	}
-	
+
+	/**
+	 * Encrypt a given string.
+	 * 
+	 * @param string_to_enc string to encrypt
+	 * @return  encrypted and Base64 encoded string
+	 * @throws InvalidKeyException
+	 * @throws InvalidAlgorithmParameterException
+	 * @throws IllegalBlockSizeException
+	 * @throws BadPaddingException 
+	 */
+	String encrypt(String string_to_enc) throws InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		// initialize cipher
+		cipher.init(Cipher.ENCRYPT_MODE, pbeKey, pbeParamSpec);
+		// encrypt string
+		byte[] encrypted = cipher.doFinal(string_to_enc.getBytes());
+		// Base64 encoding
+		String encoded = Base64.getEncoder().encodeToString(encrypted);
+		return encoded;
+	}
+
+	/**
+	 * Decrypt a string with the given password.
+	 * 
+	 * @param encoded  string to be decoded
+	 * @return  decoded string
+	 * @throws InvalidKeyException
+	 * @throws InvalidAlgorithmParameterException
+	 * @throws IllegalBlockSizeException
+	 * @throws BadPaddingException 
+	 */
+	String decrypt(String encoded) throws InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		// Base64 decoding
+		byte[] encrypted = Base64.getDecoder().decode(encoded);
+		// initialize cipher
+		cipher.init(Cipher.DECRYPT_MODE, pbeKey, pbeParamSpec);
+		// decrypt
+		byte[] decrypted = cipher.doFinal(encrypted);
+		return new String(decrypted);
+	}
 }
